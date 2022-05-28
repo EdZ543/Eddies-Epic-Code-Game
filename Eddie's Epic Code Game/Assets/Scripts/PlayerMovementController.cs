247d12af5d19e66dde4d4561cf6d07cc59095843
EdZ543 <zhuang.eddie@gmail.com>
2022-05-28 14:05:01 -0400

    Sussy movement achieved.

--- Eddie's Epic Code Game/Assets/Scripts/PlayerMovementController.cs
+++ Eddie's Epic Code Game/Assets/Scripts/PlayerMovementController.cs
@@ -6,6 +6,11 @@ public class PlayerMovementController : MonoBehaviour
 {
     public Grid grid;
 
+    Animator animator;
+
+    public int direction = 0;
+
+    private string[] directions = { "br", "bl", "tl", "tr" };
     private float stepWidth;
     private float stepHeight;
 
@@ -13,27 +18,81 @@ public class PlayerMovementController : MonoBehaviour
     {
         stepWidth = grid.cellSize.x;
         stepHeight = grid.cellSize.y;
-
-        Debug.Log(stepWidth);
+        animator = GetComponent<Animator>();
     }
 
     void moveTopLeft()
     {
-
+        transform.Translate(-stepWidth, stepHeight, 0);
     }
 
     void moveTopRight()
     {
-
+        transform.Translate(stepWidth, stepHeight, 0);
     }
 
     void moveBottomLeft()
     {
-
+        transform.Translate(-stepWidth, -stepHeight, 0);
     }
 
     void moveBottomRight()
     {
+        transform.Translate(stepWidth, -stepHeight, 0);
+    }
 
+    void turnRight()
+    {
+        direction = (direction + 1) % directions.Length;
+
+        animator.SetBool("Turn Right", true);
+    }
+
+    void turnLeft()
+    {
+        direction = direction - 1;
+        if (direction < 0)
+        {
+            direction = directions.Length - 1;
+        }
+
+        animator.SetBool("Turn Left", true);
+    }
+
+    void moveForward()
+    {
+        switch (directions[direction])
+        {
+            case "br":
+                moveBottomRight();
+                break;
+            case "bl":
+                moveBottomLeft();
+                break;
+            case "tl":
+                moveTopLeft();
+                break;
+            case "tr":
+                moveTopRight();
+                break;
+         }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            turnRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            turnLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveForward();
+        }
     }
 }
